6c644b6ddbecd528036cb903de3f4572fb4ab413
Mohd Shaib <shaj.native@gmail.com>
2020-10-31 14:43:41 +0530

    changes done 0

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,66 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "../styles/App.css";
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
-    top: "0px",
+    top: "0px"
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  const reset = () => {
+    setX(0);
+    setY(0);
+    setBallPosition({ left: 0 + "px", top: 0 + "px" });
+    setRenderBall(false);
+  };
+  const handleKeyDown = (event) => {
+    var x1 = x;
+    var y1 = y;
+    if (event.key === "ArrowRight") {
+      x1 += 5;
+      setX(x1);
+    } else if (event.key === "ArrowLeft") {
+      x1 -= 5;
+      setX(x1);
+    } else if (event.key === "ArrowUp") {
+      y1 -= 5;
+      setY(y1);
+    } else if (event.key === "ArrowDown") {
+      y1 += 5;
+      setY(y1);
+    }
+  };
+  const buttonClickHandler = () => {
+    setRenderBall(true);
+  };
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+
+    setBallPosition({ left: x + "px", top: y + "px" });
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [x, y]);
+  const renderChoice = () => {
+    if (renderBall) {
+      return <div className="ball" style={ballPosition}></div>;
+    } else
+      return (
+        <button onClick={buttonClickHandler} className="start">
+          Start
+        </button>
+      );
+  };
 
   return (
     <div className="playground">
